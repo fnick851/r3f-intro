c7f7e8b8fdd52e022a80dabe6dacb664a7485b28
Noah <hanlun.song@gmail.com>
2021-05-10 16:25:23 -0400

    fixing types

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,18 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Canvas } from "@react-three/fiber"
+import React from "react"
+import { Vector3 } from "three"
+import "./App.css"
+import { Box } from "./Box"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="app-root">
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={new Vector3(-1.2, 0, 0)} />
+        <Box position={new Vector3(1.2, 0, 0)} />
+      </Canvas>
     </div>
   )
 }
---
+++ src/Box.tsx
@@ -0,0 +1,26 @@
+import { useFrame } from "@react-three/fiber"
+import React, { useRef, useState } from "react"
+import { Vector3 } from "three"
+
+function Box(props: { position: Vector3 }) {
+  const mesh = useRef()
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  )
+}
+
+export { Box }
